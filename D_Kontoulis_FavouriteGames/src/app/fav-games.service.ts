84eea6f1e71633375cba322d6e26018d3fbdf7ed
denniskontoulis123 <w0798883@myscc.ca>
2024-03-24 10:15:54 -0400

    adjusting fav games service per instructions

--- D_Kontoulis_FavouriteGames/src/app/fav-games.service.ts
+++ D_Kontoulis_FavouriteGames/src/app/fav-games.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Content } from './helper-files/content-interface';
+import { tap } from 'rxjs/operators';
 import { contentArray } from './helper-files/contentDb';
 import { MessageService } from './services/messages/messages.service';
 
@@ -8,17 +10,43 @@ import { MessageService } from './services/messages/messages.service';
   providedIn: 'root'
 })
 export class FavGamesService {
+  private apiUrl = 'api/content';
 
-  constructor(private messageService: MessageService) { }
-
+  constructor(
+    private http: HttpClient, 
+    private messageService: MessageService
+  ) { }
+  
+  // get content from sim server
   getContentArray(): Observable<Content[]> {
     this.messageService.add('Content array loaded!');
-    return of(contentArray);
+    return this.http.get<Content[]>(this.apiUrl);
   }
 
-  getContentById(id: number): Observable<Content | undefined> {
-    const content = contentArray.find(c => c.id === id);
+  // content by id
+  getContentById(id: number): Observable<Content> {
     this.messageService.add(`Content Item at id: ${id} retrieved`);
-    return of(content);
+    return this.http.get<Content>(`${this.apiUrl}/${id}`);    
+  }
+
+  // new content item to server
+  addContent(content: Content): Observable<Content> {
+    return this.http.post<Content>(this.apiUrl, content).pipe(tap(() => {
+      this.messageService.add(`New content added with title: ${content.title}`);
+    }));
+  }
+
+  // update existing content
+  updateContent(content: Content): Observable<any> {
+    return this.http.put(this.apiUrl, content).pipe(tap(() => {
+      this.messageService.add(`Content updated with id: ${content.id}`);
+    }));
+  }
+
+  // delete content via ID
+  deleteContent(id: number): Observable<Content> {
+    return this.http.delete<Content>(`${this.apiUrl}/${id}`).pipe(tap(() => {
+      this.messageService.add(`Content deleted with id: ${id}`);
+    }));
   }
 }
