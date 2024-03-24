fd185fde84c8b9276473820793b3e59fdb404ce7
denniskontoulis123 <w0798883@myscc.ca>
2024-03-24 09:36:28 -0400

    updating + getting this ready still

--- D_Kontoulis_FavouriteGames/src/app/services/in-memory-data.service.ts
+++ D_Kontoulis_FavouriteGames/src/app/services/in-memory-data.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Content } from '../helper-files/content-interface';
+import { contentArray } from '../helper-files/contentDb';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const content : Content[] = contentArray;
+    return {content};
+  }
 
-  constructor() { }
+  genId(content: Content[]): number {
+    return content.length > 0 ?
+    Math.max(...content.map(c => c.id)) + 1 : 2000;
+  }
 }
